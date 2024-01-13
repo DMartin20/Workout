9433fd62d8b8c12ed5233aa7becc8927d386ca7d
DMartin20 <dobi.vv@gmail.com>
2024-01-13 20:17:44 +0100

    Fronpage

--- Angular/kaliworkout/src/app/components/navigation/navigation.component.ts
+++ Angular/kaliworkout/src/app/components/navigation/navigation.component.ts
@@ -1,7 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
@@ -9,11 +6,10 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
-  private breakpointObserver = inject(BreakpointObserver);
+  isSidebarOpen = false;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+  
 }
